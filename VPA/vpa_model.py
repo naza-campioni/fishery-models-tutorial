71b626091538f35b751d194acc09b3482a6ae183
naza-campioni <56881474+naza-campioni@users.noreply.github.com>
2025-08-04 09:16:49 +0200

    Update vpa_model.py

--- VPA/vpa_model.py
+++ VPA/vpa_model.py
@@ -44,19 +44,19 @@ def calculate_N(M, C_ay, N_AY):
     N_ay (np.array) - estimated population-at-age, shape [ages, years]
   """
 
-a, y = C_ay.shape
-N_ay = np.zeros([a,y])
-N_ay[:,-1] = N_AY.flatten() # survivors
-
-# the last age group is tricky - it'd need a_MAX + 1 which doesn't
-# exist, so we assume all age classes beyond a_MAX are just a_MAX
-
-for i in reversed(range(y-1)):
-  for j in reversed(range(a)):
-    if j == a-1: # if at a_MAX
-      N_ay[j,i] = N_ay[j,i+1]*np.exp(M) + C_ay[j,i]*np.exp(M/2)
-    else:
-      N_ay[j,i] = N_ay[j+1,i+1]*np.exp(M) + C_ay[j,i]*np.exp(M/2)
+  a, y = C_ay.shape
+  N_ay = np.zeros([a,y])
+  N_ay[:,-1] = N_AY.flatten() # survivors
+  
+  # the last age group is tricky - it'd need a_MAX + 1 which doesn't
+  # exist, so we assume all age classes beyond a_MAX are just a_MAX
+  
+  for i in reversed(range(y-1)):
+    for j in reversed(range(a)):
+      if j == a-1: # if at a_MAX
+        N_ay[j,i] = N_ay[j,i+1]*np.exp(M) + C_ay[j,i]*np.exp(M/2)
+      else:
+        N_ay[j,i] = N_ay[j+1,i+1]*np.exp(M) + C_ay[j,i]*np.exp(M/2)
 
 
 
@@ -75,19 +75,19 @@ def calculate_N_vectorized(M, C_ay, N_AY):
     N_ay (np.array) - estimated population-at-age, shape [ages, years]
   """
 
-  a, y = C_ay.shape
-  N_ay = np.zeros([a,y])
-  N_ay[:,-1] = N_AY.flatten() # survivors
-
-  # the last age group is tricky - it'd need a_MAX + 1 which doesn't
-  # exist, so we assume all age classes beyond a_MAX are just a_MAX
+    a, y = C_ay.shape
+    N_ay = np.zeros([a,y])
+    N_ay[:,-1] = N_AY.flatten() # survivors
   
-  for i in reversed(range(y-1)):
-    # calculate from penultimate age class backwards
-    N_ay[:-1,i] = N_ay[1:,i+1] * np.exp(M) + C_ay[:-1,i].T * np.exp(M/2)
-  
-    # then at the last age we'd need N_ay[a_MAX + 1, y] so we just use N_ay[a_MAX]
-    N_ay[-1,i] = N_ay[-1, i+1] * np.exp(M) + C_ay[-1,i].T * np.exp(M/2)
+    # the last age group is tricky - it'd need a_MAX + 1 which doesn't
+    # exist, so we assume all age classes beyond a_MAX are just a_MAX
+    
+    for i in reversed(range(y-1)):
+      # calculate from penultimate age class backwards
+      N_ay[:-1,i] = N_ay[1:,i+1] * np.exp(M) + C_ay[:-1,i].T * np.exp(M/2)
+    
+      # then at the last age we'd need N_ay[a_MAX + 1, y] so we just use N_ay[a_MAX]
+      N_ay[-1,i] = N_ay[-1, i+1] * np.exp(M) + C_ay[-1,i].T * np.exp(M/2)
   
 
   
